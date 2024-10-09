220b638a58e8ed92ea21945399daf4002b0c0a71
GrafikkartenGerd <erjonsejdiu@outlook.com>
2024-10-09 10:22:17 +0200

    Adding reciever.py as well as data reader and pcap_writer in sender.py

--- sender.py
+++ sender.py
@@ -1,7 +1,17 @@
+from itertools import count
+from scapy.all import sniff, wrpcap
+
 import dns.message
 import dns.query
 import dns.name
 import base64
+
+def read_data ():
+    file = open("secret_file.txt" , "r")
+    file.read()
+    file.close()
+
+    #Hashing the date do ensure data integrity
 
 
 def send_hidden_message(dns_server, hidden_message):
@@ -19,8 +29,12 @@ def send_hidden_message(dns_server, hidden_message):
     except Exception as e:
         print(f"Fehler beim Senden der DNS-Anfrage: {e}")
 
+def pcap_writer():
+    packets = sniff(iface= "eth0" ,filter="dns", count=100)
+    wrpcap("send_data.pcap", packets)
 
 if __name__ == "__main__":
+    read_data()
     dns_server = "127.0.0.1"
     hidden_message = "Geheime Nachricht"
     send_hidden_message(dns_server, hidden_message)
